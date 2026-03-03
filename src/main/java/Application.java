162cbac3cff406618362a50f7cc192ca2f019495
dahxxn <dahyeonhan4529@gmail.com>
2026-03-03 23:03:27 +0900

    feat(실험1): 테스트 케이스 6개로 늘리기

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,6 +6,10 @@ public class Application {
         runCase("10,K");      // 예상 결과: 20 (10 + 10)
         runCase("A,9");       // 예상 결과: 20 (11 + 9)
         runCase("2,3,4");     // 예상 결과: 9 (2 + 3 + 4)
+
+        runCase("J,Q");       // 예상 결과: 20
+        runCase("1,8,9");     // 예상 결과: x (1이란 카드는 없음)
+        runCase("A,K,9");
     }
 
     private static void runCase(String testCase) {
