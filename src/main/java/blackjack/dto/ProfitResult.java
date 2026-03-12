69d22684284011b0b5ec9e84394d9ffb8703eda5
dahxxn <dahyeonhan4529@gmail.com>
2026-03-12 16:08:43 +0900

    refactor: ProfitResult, PlayerProfitResult double -> int로 타입 변경

--- src/main/java/blackjack/dto/ProfitResult.java
+++ src/main/java/blackjack/dto/ProfitResult.java
@@ -2,6 +2,6 @@ package blackjack.dto;
 
 import java.util.List;
 
-public record ProfitResult(double dealerProfit, List<PlayerProfitResult> playerProfits) {
+public record ProfitResult(int dealerProfit, List<PlayerProfitResult> playerProfits) {
 }
 
