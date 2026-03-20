45374f210c5a1ffb83fe6e1f1c23bc7f0cf901be
dahxxn <dahyeonhan4529@gmail.com>
2026-03-20 13:51:32 +0900

    feat: Hand 객체 구현

---
+++ src/main/java/domain/Hand.java
@@ -0,0 +1,27 @@
+package domain;
+
+import java.util.List;
+
+public class Hand {
+    private List<Card> cards;
+
+    private Hand(List<Card> initialCards) {
+        validate(initialCards);
+        this.cards = initialCards;
+    }
+
+    public static Hand of(List<Card> cards) {
+        return new Hand(cards);
+    }
+
+    private void validate(List<Card> cards) {
+        validateNotNull(cards);
+    }
+
+    private void validateNotNull(List<Card> cards) {
+        if (cards == null) {
+            throw new IllegalArgumentException("null 이 올 수 없습니다.");
+        }
+    }
+
+}
---
+++ src/test/java/domain/HandTest.java
@@ -0,0 +1,25 @@
+package domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class HandTest {
+
+    @Test
+    void Card_리스트로_Hand를_생성할_수_있다() {
+        Hand hand = Hand.of(List.of(Card.of(Suits.HEART, Rank.ACE), Card.of(Suits.DIAMOND, Rank.TWO)));
+        assertThat(hand).isNotNull();
+    }
+
+    @Test
+    void Card_리스트_null이면_Hand_생성시_오류가_발생한다() {
+        assertThatThrownBy(() -> {
+            Hand.of(null);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+}
